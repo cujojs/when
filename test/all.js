2c2e388de998b0c24071ddaa18779865085d3bd3
Brian Cavalier <brian@briancavalier.com>
2011-07-27 13:41:13 -0400

    Adding fake unit test to workaround a problem in DOH test harness that causes the first test to always fail.

--- test/all.js
+++ test/all.js
@@ -1,3 +1,8 @@
+// DOH seems to faily consistently on the first test suite, so I'm putting
+// in this fake suite so it will fail and all the real tests results will
+// be meaningful.
+doh.registerUrl('_fake', '../../_fake-doh.html');
+
 doh.registerUrl('when.isPromise', '../../isPromise.html');
 doh.registerUrl('when', '../../when.html');
 doh.registerUrl('when.Deferred', '../../Deferred.html');
