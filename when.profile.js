4bca0c947aacf50d7bd0090c1ac5da9dd1cd4e18
Bryan Forbes <bryan@reigndropsfall.net>
2011-12-23 11:29:37 -0600

    Fix tabs and spaces.

--- when.profile.js
+++ when.profile.js
@@ -2,19 +2,19 @@ var profile = (function(){
 	var testRE = /^when\/test\//;
 	return {
 		resourceTags: {
-            test: function(filename, mid){
+			test: function(filename, mid){
 				// Tag test files as such
-                return testRE.test(mid);
-            },
-            amd: function(filename, mid){
+				return testRE.test(mid);
+			},
+			amd: function(filename, mid){
 				// Tag the module as AMD so it doesn't get
 				// wrapped by the Dojo builder
-                return mid == "when/when" || mid == "when";
-            },
-            copyOnly: function(filename, mid){
+				return mid == "when/when" || mid == "when";
+			},
+			copyOnly: function(filename, mid){
 				// Don't process package.json
-                return mid == "when/package.json";
-            }
+				return mid == "when/package.json";
+			}
 		}
 	};
 })();
