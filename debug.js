7c0bb3e5519c38e1bb69b18898ae7eeea51a775e
Brian Cavalier <brian@hovercraftstudios.com>
2012-03-09 08:12:20 -0500

    Adding more toString detail, including an optional id, and setting up output for resolve() and reject()

--- debug.js
+++ debug.js
@@ -1,44 +1,197 @@
 /** @license MIT License (c) copyright B Cavalier & J Hann */
 
+/**
+ * This is a drop-in replacement for the when module that sets up automatic
+ * debug output for promises created or consumed by when.js.  Use this
+ * instead of when to help with debugging.
+ *
+ * WARNING: This module **should never** be use this in a production environment.
+ * It exposes details of the promise
+ *
+ * In an AMD environment, you can simply change your path or package mappings:
+ *
+ * paths: {
+ *   // 'when': 'path/to/when/when'
+ *   'when': 'path/to/when/debug'
+ * }
+ *
+ * or
+ *
+ * packages: [
+ *   // { name: 'when', location: 'path/to/when', main: 'when' }
+ *   { name: 'when', location: 'path/to/when', main: 'debug' }
+ * ]
+ *
+ * In a CommonJS environment, you can directly require this module where
+ * you would normally require 'when':
+ *
+ * // var when = require('when');
+ * var when = require('when/debug');
+ *
+ * Or you can temporarily modify the package.js to point main at debug.
+ * For example, when/package.json:
+ *
+ * ...
+ * "main": "./debug"
+ * ...
+ *
+ * @author brian@hovercraftstudios.com
+ */
 (function(define) {
 define(['./when'], function(when) {
 
+	var promiseId, freeze, pending, undef;
+
+	promiseId = 0;
+	freeze = Object.freeze || function(o) { return o; };
+	pending = {};
+
+	/**
+	 * Setup debug output handlers for the supplied promise.
+	 * @param p {Promise} A trusted (when.js) promise
+	 * @return p
+	 */
 	function debugPromise(p) {
 		// TODO: Need to find a way for promises returned by .then()
 		// to also be debug promises.
 		p.then(
 			function(val) {
-				console.log('[object Promise] resolved', val);
+				if(p.id) {
+					console.log(p.toString());
+				} else {
+					console.log('[object Promise] resolved:', val);
+				}
 			},
 			function(err) {
-				console.error('[object Promise] REJECTED', err);
+				if(p.id) {
+					console.err(p);
+				} else {
+					console.error('[object Promise] REJECTED:', err);
+				}
 			}
 		);
 
 		return p;
 	}
 
+	/**
+	 * Helper to form debug string for promises depending on their
+	 * current state
+	 * @param name
+	 * @param id
+	 * @param status
+	 * @param value
+	 */
+	function toString(name, id, status, value) {
+		var s = '[object ' + name + ' ' + id + '] ' + status;
+		if(value !== pending) s += ': ' + value;
+		return s;
+	}
+
+	function F() {}
+	function beget(o) {
+		F.prototype = o;
+		o = new F();
+		F.prototype = undef;
+
+		return o;
+	}
+
+	/**
+	 * Replacement for when() that sets up debug logging on the
+	 * returned promise.
+	 */
 	function whenDebug() {
 		return debugPromise(when.apply(null, arguments));
 	}
 
-	function defer() {
-		var d = when.defer();
+	/**
+	 * Replacement for when.defer() that sets up debug logging
+	 * on the created Deferred, its resolver, and its promise.
+	 * @param [id] anything optional identifier for this Deferred that will show
+	 * up in debug output
+	 * @return {Deferred} a Deferred with debug logging
+	 */
+	function deferDebug(id) {
+		var d, status, value, origResolve, origReject;
 
+		// Delegate to create a Deferred;
+		d = when.defer();
+
+		status = 'pending';
+		value = pending;
+
+		// if no id provided, generate one.  Not sure if this is
+		// useful or not.
+		if(arguments.length == 0) id = ++promiseId;
+
+		// Promise and resolver are frozen, so have to delegate
+		// in order to setup toString() on promise, resolver,
+		// and deferred
+		d.promise = beget(d.promise);
+		d.promise.toString = function() {
+			return toString('Promise', id, status, value);
+		};
+
+		d.resolver = beget(d.resolver);
+		d.resolver.toString = function() {
+			return toString('Resolver', id, status, value);
+		};
+
+		origResolve = d.resolver.resolve;
+		d.resolve = d.resolver.resolve = function(val) {
+			value = val;
+			status = 'resolving';
+			console.log(toString('Resolver', id, status, val));
+			return origResolve.apply(undef, arguments);
+		};
+
+		origReject = d.resolver.reject;
+		d.reject = d.resolver.reject = function(err) {
+			value = err;
+			status = 'REJECTING';
+			console.log(toString('Resolver', id, status, err));
+			return origReject.apply(undef, arguments);
+		};
+
+		d.toString = function() {
+			return toString('Deferred', id, status, value);
+		};
+
+		// Setup final state change handlers
+		d.then(
+			function() { status = 'resolved'; },
+			function() { status = 'REJECTED'; }
+		);
+
+		// Add an id to all directly created promises.  It'd be great
+		// to find a way to propagate this id to promise created by .then()
+		d.id = d.promise.id = d.resolver.id = id;
+
+		// Attach debug handlers after the substitute promise
+		// has been setup, so the id can be logged.
 		debugPromise(d.promise);
+
+		// TODO: Should we still freeze these?
+		// Seems safer for now to err on the side of caution and freeze them,
+		// but it could be useful to all them to be modified during debugging.
+		freeze(d.promise);
+		freeze(d.resolver);
 
 		return d;
 	}
 
-	whenDebug.defer = defer;
+	whenDebug.defer = deferDebug;
 	whenDebug.isPromise = when.isPromise;
 
+	// For each method we haven't already replaced, replace it with
+	// one that sets up debug logging on the returned promise
 	for(var p in when) {
 		if(when.hasOwnProperty(p) && !(p in whenDebug)) {
 			(function(p, orig) {
 				whenDebug[p] = function() {
 					return debugPromise(orig.apply(when, arguments));
-				}
+				};
 			})(p, when[p]);
 		}
 	}
--- test/promise.js
+++ test/promise.js
@@ -290,7 +290,7 @@ buster.testCase('promise', {
 		);
 
 		d.reject(1);
-	},
+	}
 
 	'should call progback': function(done) {
 		var expected, d;
