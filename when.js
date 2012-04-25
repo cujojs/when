b2e06d19e53e842a21d6bb96dabf166dcbc75250
Brian Cavalier <brian@hovercraftstudios.com>
2012-04-25 13:31:04 -0400

    Fix for calling any without a callback

--- when.js
+++ when.js
@@ -537,7 +537,7 @@ define(function() {
     function any(promisesOrValues, callback, errback, progressHandler) {
 
         function unwrapSingleResult(val) {
-            return callback(val[0]);
+            return callback ? callback(val[0]) : val[0];
         }
 
         return some(promisesOrValues, 1, unwrapSingleResult, errback, progressHandler);
