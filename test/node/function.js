9243c16af2777e4703dfd79743707abeea69b0fe
Renato Zannon <renato.riccieri@gmail.com>
2012-12-18 20:59:08 -0200

    Add tests for node_fn.call
    That are not really different from the ones for 'node_fn.apply'... Is
    that a problem?

--- test/node/function.js
+++ test/node/function.js
@@ -60,6 +60,60 @@ buster.testCase('when/node/function', {
 				assert.equals(value, 30);
 			}).always(done);
 		}
+	},
+
+	'call': {
+		'should return promise': function() {
+			var result = node_fn.call(function() {});
+			assertIsPromise(result);
+		},
+
+		'the returned promise': {
+			'should be resolved with the 2nd arg to the callback': function(done) {
+				function async(cb) {
+					cb(null, 10);
+				}
+
+				var promise = node_fn.call(async);
+				promise.then(function(value) {
+					assert.equals(value, 10);
+				}, fail).always(done);
+			},
+
+			'should be rejected with the 1st arg to the callback': function(done) {
+				var error = new Error('foobar');
+				function async(cb) {
+					cb(error);
+				}
+
+				var promise = node_fn.call(async);
+				promise.then(fail, function(reason) {
+					assert.same(reason, error);
+				}).always(done);
+			},
+
+			'should be resolved to an array for multi-arg callbacks': function(done) {
+				function async(cb) {
+					cb(null, 10, 20, 30);
+				}
+
+				var promise = node_fn.call(async);
+				promise.then(function(values) {
+					assert.equals(values, [10, 20, 30]);
+				}).always(done);
+			}
+		},
+
+		'should forward extra arguments to the function': function(done) {
+			function async(x, y, cb) {
+				cb(null, x + y);
+			}
+
+			var promise = node_fn.call(async, 10, 20);
+			promise.then(function(value) {
+				assert.equals(value, 30);
+			}).always(done);
+		}
 	}
 });
 })(require('buster'), require('../../node/function'), require('../../when'));
