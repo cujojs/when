dcc0e678d02e5b42134a672c854d239628338e42
Renato Riccieri <renato.riccieri@gmail.com>
2013-02-03 20:30:12 -0200

    Document callbacks.apply

--- callbacks.js
+++ callbacks.js
@@ -4,6 +4,37 @@ define(['./when'], function(when) {
 		apply: apply
 	};
 
+	/**
+	* Takes a `traditional` callback-taking function and returns a promise for its
+	* result, accepting an optional array of arguments. It assumes that the
+	* function takes its callback and errback as the last two arguments. The
+	* resolution of the promise depends on whether the function will call its
+	* callback or its errback.
+	*
+	* @example
+	*	var domIsLoaded = callbacks.apply($);
+	*	domIsLoaded.then(function() {
+	*		doMyDomStuff();
+	*	});
+	*
+	* @example
+	*	function existingAjaxyFunction(url, callback, errback) {
+	*		// Complex logic you'd rather not change
+	*	}
+	*
+	*	var promise = callbacks.apply(existingAjaxyFunction, ["/movies.json"]);
+	*
+	* promise.then(function(movies) {
+	*   // Work with movies
+	* }, function(reason) {
+	*		// Handle error
+	* });
+	*
+	* @param {function} asyncFunction function to be called
+	* @param {Array} [extraAsyncArgs] array of arguments to asyncFunction
+	* @returns {Promise} promise for the callback value of asyncFunction
+	*/
+
 	function apply(asyncFunction, extraAsyncArgs) {
 		if(typeof extraAsyncArgs === 'undefined') {
 			extraAsyncArgs = [];
