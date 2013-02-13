b4a00be6e9fcbd08273e98f69f8283b8bc9373bb
Sakari Jokinen <sakari.jokinen@rocketpack.fi>
2013-02-13 15:36:36 +0200

    guard using a condition object

--- guard.js
+++ guard.js
@@ -1,4 +1,4 @@
-/** @license MIT License (c)
+/** @license MIT License (c) copyright B Cavalier & J Hann */
 
 /**
  * guard.js
@@ -6,33 +6,35 @@
  * Wait until the guard condition is true before proceeding
  * with deferred execution.
  *
- * @author Rocket Pack Ltd.
+ * @author sakari@rocketpack.fi
  */
 
 (function(define) {
 define(['./when', './function'], function(when, func) {
 
 	function Guard(condition) {
-		if (condition instanceof Function) {
+		if (condition.enter) {
 			this._condition = condition;
 		} else {
 			var users = 0;
-			this._condition = function(t) {
-				if(t === 'enter' && users < condition) {
-					users++;
-					return true;
-				}
-				if(t === 'exit') {
+			this._condition = {
+				enter: function() {
+					if(users < condition) {
+						users++;
+						return true;
+					}
+					return false;
+				},
+				exit: function() {
 					users--;
 				}
-				return false;
 			};
 		}
 		this._pending = [];
 	}
 	Guard.prototype._trigger = function() {
 		var self = this;
-		if(this._pending.length === 0 || !this._condition('enter')) {
+		if(this._pending.length === 0 || !this._condition.enter()) {
 			return;
 		}
 		var next = this._pending.shift();
@@ -43,7 +45,7 @@ define(['./when', './function'], function(when, func) {
 				next.dfd.reject.apply(next.dfd, Array.prototype.slice.apply(arguments));
 			})
 			.always(function() {
-				self._condition('exit');
+				self._condition.exit();
 				self._trigger();
 			});
 	};
--- test/guard.js
+++ test/guard.js
@@ -9,54 +9,40 @@ fail = buster.assertions.fail;
 resolved = when.resolve;
 rejected = when.reject;
 
-
 buster.testCase('when/guard', {
 	'should be usable for throttling when.map': function(done) {
-		var called = [];
-		var dfds = [when.defer(), when.defer(), when.defer(), when.defer()];
+		var dfds = [0, 1, 2, 3].map(function() {
+			return when.defer();
+		});
 		when.map([0, 1, 2, 3], guard(2).do(function(v) {
-			called.push(v);
-			return dfds[v];
-		}));
-		assert.equals(called, [0, 1]);
-		dfds[1].resolve();
-		assert.equals(called, [0, 1, 2]);
-		done();
+			return dfds[v].resolve(v * 10);
+		})).then(function(v) {
+			assert.equals(v, [0, 10, 20, 30]);
+			done();
+		});
 	},
 	'should guard the function with `enter` and `exit` calls': function(done) {
 		var got = [];
-		function condition(t) {
-			got.push(t);
-			return true;
-		}
-		var g = guard(condition);
+		var g = guard({
+			enter: function() { got.push('enter'); return true; },
+			exit: function() { got.push('exit'); }
+		});
 		g.do(function() {
 			return when.defer().resolve();
-		})();
-		assert.equals(got, ['enter', 'exit']);
-		done();
-	},
-	'should prevent execution of guarded function if condition returns false': function(done) {
-		var called;
-		function condition() {
-			called = true;
-			return false;
-		}
-		guard(condition).do(function() {
-			throw new Error('should not be called');
-		})();
-		assert.equals(called, true);
-		done();
+		})().then(function() {
+			assert.equals(got, ['enter', 'exit']);
+			done();
+		});
 	},
 	'should convert normal functions to return deferreds': function(done) {
-		guard(function() { return true; }).do(function() { return 1; })()
+		guard(1).do(function() { return 1; })()
 			.then(function(v) {
 				assert.equals(v, 1);
 				done();
 			});
 	},
 	'should allow deferred arguments': function(done) {
-		var g = guard(function() { return true; }).do(function(v) { return v; });
+		var g = guard(1).do(function(v) { return v; });
 		g(when.defer().resolve(1))
 			.then(function(v) {
 				assert.equals(v, 1);
@@ -65,58 +51,44 @@ buster.testCase('when/guard', {
 	},
 	'should call exit even when the function throws': function(done) {
 		var got = [];
-		guard(function(t) { got.push(t); return true; })
+		var condition = {
+			exit: function() { got.push('exit'); },
+			enter: function() { got.push('enter'); return true;}
+		};
+		guard(condition)
 			.do(function() { throw new Error(); })()
 			.then(null, function() {
 				assert.equals(got, ['enter', 'exit']);
 				done();
 			});
 	},
-	'should understand shorthand for limiting concurrent users ': function(done) {
+	'should be sharable between functions': function(done) {
+		var g = guard(1);
+		var first;
+		var d = when.defer();
+		g.do(function() {
+			first = true;
+			return d;
+		})();
+		g.do(function() {
+			assert.equals(first, true);
+			done();
+		})();
+		d.resolve();
+	},
+	'should limit concurrent users using a shorthand': function(done) {
 		var calls = [];
-		var dfds = [when.defer(), when.defer()];
+		var first = when.defer();
+		var dfds = [first.then(function() { assert.equals(calls, [0]); }),
+					when.defer().resolve()];
 		var g = guard(1).do(function(v) { calls.push(v); return dfds[v]; });
 		g(0);
-		g(1);
-		assert.equals(calls, [0]);
-		dfds[0].resolve();
-		assert.equals(calls, [0, 1]);
-		done();
-	},
-	'should allow multiple concurrent users when limited using shorthand': function(done) {
-		var calls = [];
-		var g = guard(2).do(function(v) { calls.push(v); return when.defer(); });
-		g(1);
-		g(2);
-		assert.equals(calls, [1, 2]);
-		done();
-	},
-	'should trigger guarded function calls when previous call finishes': function(done) {
-		var occupied;
-		function condition(t) {
-			if(t === 'enter') {
-				if(occupied) {
-					return false;
-				}
-				occupied = true;
-				return true;
-			}
-			occupied = false;
-		}
-		var dfds = [when.defer(), when.defer()];
-		var calls = [];
-		var fn = guard(condition).do(function(v) {
-			calls.push(v);
-			return dfds[v];
+		g(1).then(function() {
+			assert.equals(calls, [0, 1]);
+			done();
 		});
-		fn(0);
-		fn(1);
-		assert.equals(calls, [0]);
-		dfds[0].resolve();
-		assert.equals(calls, [0, 1]);
-		done();
+		first.resolve();
 	}
-
 });
 })(
 	this.buster || require('buster'),
