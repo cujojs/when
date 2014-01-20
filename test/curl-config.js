4d5f979c3ac71d5580f971843255aa7232767ef7
Brian Cavalier <brian@hovercraftstudios.com>
2014-01-20 08:15:25 -0500

    Test using es5-shim for legacy browser testing support

--- test/curl-config.js
+++ test/curl-config.js
@@ -5,7 +5,8 @@
 		packages: [
 			{ name: 'when', location: './', main: 'when' },
 			{ name: 'curl', location: 'node_modules/curl/src/curl', main: 'curl' }
-		]
+		],
+		preloads: ['//cdnjs.cloudflare.com/ajax/libs/es5-shim/2.2.0/es5-shim.min']
 	};
 
 }(this));
