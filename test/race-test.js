5690275677f8c879f677a281cc5731aa47194033
Brian Cavalier <brian@hovercraftstudios.com>
2014-01-27 09:54:06 -0500

    Add Promise.race tests

---
+++ test/race-test.js
@@ -0,0 +1,91 @@
+(function(buster, define) {
+
+var assert, fail;
+
+assert = buster.assert;
+fail = buster.assertions.fail;
+
+define('when/race-test', function (require) {
+
+	var Promise = require('when/Promise');
+	var sentinel = { value: 'sentinel' };
+	var fulfilled = Promise.of(sentinel);
+	var rejected = Promise.reject(sentinel);
+	var empty = Promise.empty();
+
+	function delayReject(ms) {
+		return Promise.of().delay(ms)['yield'](rejected)
+	}
+
+	buster.testCase('Promise.race', {
+		'should return empty race for length 0': function() {
+			assert.equals(empty, Promise.race([]));
+		},
+
+		'should be identity for length 1': {
+			'when fulfilled with value': function() {
+				return Promise.race([sentinel]).then(function(x) {
+					assert.same(x, sentinel);
+				});
+			},
+
+			'when fulfilled via promise': function() {
+				return Promise.race([fulfilled]).then(function(x) {
+					assert.same(x, sentinel);
+				});
+			},
+
+			'when rejected': function() {
+				return Promise.race([rejected])
+					.then(void 0, function(x) {
+						assert.same(x, sentinel);
+					});
+			}
+		},
+
+		'should be commutative': {
+			'when fulfilled': function() {
+				return Promise.race([fulfilled, empty]).then(function(x) {
+					return Promise.race([empty, fulfilled]).then(function(y) {
+						assert.same(x, y);
+					});
+				});
+			},
+
+			'when rejected': function() {
+				return Promise.race([rejected, empty]).then(void 0, function(x) {
+					return Promise.race([empty, rejected]).then(void 0, function(y) {
+						assert.same(x, y);
+					});
+				});
+			}
+		},
+
+		'should fulfill when winner fulfills': function() {
+			return Promise.race([delayReject(1), delayReject(1), fulfilled])
+				.then(function(x) {
+					assert.same(x, sentinel);
+				}, fail);
+		},
+
+		'should reject when winner rejects': function() {
+			return Promise.race([fulfilled.delay(1), fulfilled.delay(1), rejected])
+				.then(fail, function(x) {
+					assert.same(x, sentinel);
+				});
+		}
+	});
+
+});
+
+}(
+	this.buster || require('buster'),
+	typeof define === 'function' && define.amd ? define : function (id, factory) {
+		var packageName = id.split(/[\/\-\.]/)[0], pathToRoot = id.replace(/[^\/]+/g, '..');
+		pathToRoot = pathToRoot.length > 2 ? pathToRoot.substr(3) : pathToRoot;
+		factory(function (moduleId) {
+			return require(moduleId.indexOf(packageName) === 0 ? pathToRoot + moduleId.substr(packageName.length) : moduleId);
+		});
+	}
+	// Boilerplate for AMD and Node
+));
