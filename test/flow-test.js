cc5f16bbe6e91b114cec23f8cce400eb83c1ac98
Brian Cavalier <brian@hovercraftstudios.com>
2014-02-21 08:45:51 -0500

    Add catch predicate support, with unit tests. Move finally unit tests to flow-test.js. Still need to move others as well. See #251

---
+++ test/flow-test.js
@@ -0,0 +1,137 @@
+(function(buster, define) {
+
+var assert, fail, sentinel, other;
+
+assert = buster.assert;
+fail = buster.assertions.fail;
+
+sentinel = { value: 'sentinel' };
+other = { value: 'other' };
+
+define('when/flow-test', function (require) {
+
+	var Promise = require('when').Promise;
+
+	buster.testCase('when/lib/flow', {
+		'otherwise': {
+			'should be an alias for catch': function() {
+				assert.same(Promise.prototype['catch'], Promise.prototype.otherwise);
+			}
+		},
+
+		'catch': {
+			'should catch rejections': function() {
+				return Promise.reject(sentinel)['catch'](function(e) {
+					assert.same(e, sentinel);
+				});
+			},
+
+			'when Error type match is specified': {
+				'should only catch errors of same type': function() {
+					var e1 = new TypeError();
+					return Promise.reject(e1)['catch'](SyntaxError, fail)
+						['catch'](TypeError, function(e) {
+						assert.same(e1, e);
+					});
+				}
+			},
+
+			'when predicate is specified': {
+				'should only catch errors of same type': function() {
+					var e1 = new TypeError();
+					return Promise.reject(e1) ['catch'](function(e) {
+							return e !== e1;
+					}, fail)['catch'](function(e) {
+						return e === e1;
+					}, function(e) {
+						assert.same(e1, e);
+					});
+				}
+			}
+		},
+
+		'finally': {
+			'should be an alias for ensure': function() {
+				var p = Promise.resolve();
+				assert.same(p['finally'], p.ensure);
+			}
+		},
+
+		'ensure': {
+			'should return a promise': function() {
+				assert.isFunction(Promise.resolve().ensure().then);
+			},
+
+			'when fulfilled': {
+				'should ignore callback return value': function() {
+					return Promise.resolve(sentinel).ensure(
+						function() {
+							return other;
+						}
+					).then(
+						function(val) {
+							assert.same(val, sentinel);
+						},
+						fail
+					);
+				},
+
+				'should propagate rejection on throw': function() {
+					return Promise.resolve(other).ensure(
+						function() {
+							throw sentinel;
+						}
+					).then(
+						fail,
+						function(val) {
+							assert.same(val, sentinel);
+						}
+					);
+				}
+			},
+
+			'when rejected': {
+				'should propagate rejection, ignoring callback return value': function() {
+					return Promise.reject(sentinel).ensure(
+						function() {
+							return other;
+						}
+					).then(
+						fail,
+						function(val) {
+							assert.same(val, sentinel);
+						}
+					);
+				},
+
+				'should propagate rejection on throw': function() {
+					return Promise.reject(other).ensure(
+						function() {
+							throw sentinel;
+						}
+					).then(
+						fail,
+						function(val) {
+							assert.same(val, sentinel);
+						}
+					);
+				}
+			},
+
+			'should ignore non-function': function() {
+				return Promise.resolve(true).ensure().then(assert);
+			}
+		}
+
+	});
+
+});
+
+}(this.buster || require('buster'),
+	typeof define === 'function' && define.amd ? define : function (id, factory) {
+	var packageName = id.split(/[\/\-\.]/)[0], pathToRoot = id.replace(/[^\/]+/g, '..');
+	pathToRoot = pathToRoot.length > 2 ? pathToRoot.substr(3) : pathToRoot;
+	factory(function (moduleId) {
+		return require(moduleId.indexOf(packageName) === 0 ? pathToRoot + moduleId.substr(packageName.length) : moduleId);
+	});
+}));
