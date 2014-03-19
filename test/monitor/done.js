96d02db571ff68846d1cd8649fd5a8bafc49371b
Brian Cavalier <brian@hovercraftstudios.com>
2014-03-18 21:06:48 -0400

    Extract promise cycle handler to function so stack traces make sense

--- test/monitor/done.js
+++ test/monitor/done.js
@@ -15,12 +15,12 @@ define(function(require) {
 
 	var when = require('../../when');
 
-	when.resolve('abc').catch(123, function(){});
-//	when.resolve(123)
-//		.then(function(x) {
-//			throw new Error(x);
-//		})
-//		.done(console.log);
+//	when.resolve('abc').catch(123, function(){});
+	when.resolve(123)
+		.then(function(x) {
+			throw new Error(x);
+		})
+		.done(console.log);
 
 });
 }(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
