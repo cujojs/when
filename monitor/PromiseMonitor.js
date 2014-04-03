1f8a9319c7ed65f444b906a087f19795c57ac937
Aria <aria.minaei@gmail.com>
2014-04-03 13:42:16 +0430

    Changed defaultStackFilter to match windows paths

--- monitor/PromiseMonitor.js
+++ monitor/PromiseMonitor.js
@@ -6,7 +6,7 @@
 define(function(require) {
 
 	var defaultStackJumpSeparator = 'from execution context:';
-	var defaultStackFilter = /(node|module|timers)\.js:|when(\/(lib|monitor|es6-shim)\/|\.js)|(new\sPromise)\b|(\b(PromiseMonitor|ConsoleReporter|Scheduler|RunHandlerTask|ProgressTask|Promise|.*Handler)\.[\w_]\w\w+\b)|\b(tryCatch\w+|getHandler\w*)\b/i;
+	var defaultStackFilter = /(node|module|timers)\.js:|when([\/\\]{1,2}(lib|monitor|es6-shim)[\/\\]{1,2}|\.js)|(new\sPromise)\b|(\b(PromiseMonitor|ConsoleReporter|Scheduler|RunHandlerTask|ProgressTask|Promise|.*Handler)\.[\w_]\w\w+\b)|\b(tryCatch\w+|getHandler\w*)\b/i;
 
 	var setTimer = require('../lib/timer').set;
 	var error = require('./error');
