378e03809664397723dd02958885e5a0dc2c25be
Jeff Escalante <hello@jenius.me>
2014-04-15 11:25:35 -0400

    remove boilerplate from test

--- test/fold-test.js
+++ test/fold-test.js
@@ -1,52 +1,29 @@
-(function(buster, define) {
+var buster = typeof window !== 'undefined' ? window.buster : require('buster');
+var assert = buster.assert;
+var when = require('../when');
 
-var assert, fail, sentinel;
+function sum(x, y) {
+	return x + y;
+}
 
-assert = buster.assert;
-fail = buster.assertions.fail;
+function equal(x, y) {
+	if (x !== y) { throw 'not equal!'; }
+}
 
-sentinel = { value: 'sentinel' };
+buster.testCase('promise.fold', {
 
-define('when/fold-test', function (require) {
-
-	var when = require('when');
-
-	function sum(x, y) {
-		return x + y;
-	}
-
-	function equal(x, y) {
-		if (x !== y) { throw 'not equal!'; }
-	}
-
-	buster.testCase('fold', {
-
-		'when calling fold on a promise': {
-			'should pairwise combine two promises': function() {
-				return when.resolve(1).fold(sum, 2).then(function(res){
-					assert.equals(res, 3);
-				});
-			}, 'should still fail normally after a fold': function() {
-				return when.resolve(1).fold(equal, 2).catch(function(res){
-					assert.equals(res, 'not equal!');
-				});
-			}, 'should reject and not call fold if previous promise rejects': function() {
-				return when.reject(1).fold(equal, 2).catch(function(res){
-					assert.equals(res, 1);
-				});
-			}
-		}
-	});
-
-});
-}(
-	this.buster || require('buster'),
-	typeof define === 'function' && define.amd ? define : function (id, factory) {
-		var packageName = id.split(/[\/\-\.]/)[0], pathToRoot = id.replace(/[^\/]+/g, '..');
-		pathToRoot = pathToRoot.length > 2 ? pathToRoot.substr(3) : pathToRoot;
-		factory(function (moduleId) {
-			return require(moduleId.indexOf(packageName) === 0 ? pathToRoot + moduleId.substr(packageName.length) : moduleId);
+	'should pairwise combine two promises': function() {
+		return when.resolve(1).fold(sum, 2).then(function(res){
+			assert.equals(res, 3);
+		});
+	}, 'should still fail normally after a fold': function() {
+		return when.resolve(1).fold(equal, 2)['catch'](function(res){
+			assert.equals(res, 'not equal!');
+		});
+	}, 'should reject and not call fold if previous promise rejects': function() {
+		return when.reject(1).fold(equal, 2)['catch'](function(res){
+			assert.equals(res, 1);
 		});
 	}
-	// Boilerplate for AMD and Node
-));
+
+});
