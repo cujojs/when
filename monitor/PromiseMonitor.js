8bbdfac23ca10349c62bee331b3663f5eec94c4f
Brian Cavalier <brian@hovercraftstudios.com>
2014-04-16 08:53:31 -0400

    Always report unhandled rejections, even w/o done()

--- monitor/PromiseMonitor.js
+++ monitor/PromiseMonitor.js
@@ -86,8 +86,7 @@ define(function(require) {
 
 	PromiseMonitor.prototype.formatTraces = function(traces) {
 		return traces.map(function(t) {
-			return this._createLongTrace(
-				t.handler.value, t.handler.context, t.extraContext);
+			return this._createLongTrace(t.handler.value, t.handler.context, t.extraContext);
 		}, this);
 	};
 
