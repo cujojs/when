6035785f4dfce2f3d22180050a12e658e5100710
Brian Cavalier <brian@hovercraftstudios.com>
2014-05-13 22:42:15 -0400

    Add node-lift test for unhandled rejections

--- test/monitor/lift-node.js
+++ test/monitor/lift-node.js
@@ -1,12 +1,10 @@
 require('../../monitor/console');
 var node = require('../../node');
-var Promise = require('bluebird');
 
 function test() {
 	throw new Error('fail');
 }
 
-//var f = Promise.promisify(test);
 var f = node.lift(test);
 
 f();
