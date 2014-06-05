af440fc481292e3e872629ecf66219e692e87d30
Brian Cavalier <brian@hovercraftstudios.com>
2014-06-04 21:50:06 -0400

    Add test for minimal delay when observing rejections, to simulate rejections being observed by another promise impl, which may not call then() (nay, *should not*) in the current stack.

---
+++ test/monitor/delay-handled.js
@@ -0,0 +1,14 @@
+(function(define) { 'use strict';
+define(function(require) {
+
+	var when = require('../../when');
+	var async = require('../../lib/async');
+
+	var p = when.reject(new Error('TEST FAILED, should not see this'));
+
+	async(function() {
+		p.catch(function(){});
+	});
+});
+}(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
+
