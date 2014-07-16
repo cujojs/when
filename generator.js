23edc6c1617f3e38dd48d2693faff0ac2a2bd585
Brian Cavalier <brian@hovercraftstudios.com>
2014-07-16 08:48:58 -0400

    Reduce mem usage slightly for lifted generators

--- generator.js
+++ generator.js
@@ -1,24 +1,12 @@
 /** @license MIT License (c) copyright 2010-2014 original author or authors */
-
-/**
- * Licensed under the MIT License at:
- * http://www.opensource.org/licenses/mit-license.php
- *
- * @author: Brian Cavalier
- * @author: John Hann
- */
+/** @author Brian Cavalier */
+/** @author John Hann */
 
 (function(define) { 'use strict';
 define(function(require) {
 
 	var when = require('./when');
 	var slice = Array.prototype.slice;
-
-	return {
-		apply: apply,
-		call: call,
-		lift: lift
-	};
 
 	/**
 	 * Lift a generator to create a function that can suspend and
@@ -28,8 +16,8 @@ define(function(require) {
 	 */
 	function lift(generator) {
 		return function() {
-			return apply(generator, arguments);
-		};
+			return run(generator, this, arguments);
+		}
 	}
 
 	/**
@@ -42,7 +30,8 @@ define(function(require) {
 	 *  from the generator.
 	 */
 	function call(generator /*x, y, z...*/) {
-		return apply(generator, slice.call(arguments, 1));
+		/*jshint validthis:true*/
+		return run(generator, this, slice.call(arguments, 1));
 	}
 
 	/**
@@ -50,34 +39,56 @@ define(function(require) {
 	 * as a promise-aware coroutine that can suspend and resume
 	 * using the `yield` keyword to await promises.
 	 * @param {function} generator
+	 * @param {Array} args arguments with which to initialize the generator
 	 * @returns {Promise} promise for the ultimate value returned
 	 *  from the generator.
 	 */
 	function apply(generator, args) {
 		/*jshint validthis:true*/
-		var iterator = generator.apply(this, args);
-
-		return next();
-
-		function next(x) {
-			return step('next', x);
-		}
-
-		function fail(e) {
-			return step('throw', e);
-		}
-
-		function step(action, x) {
-			var result;
-
-			try {
-				result = iterator[action](x);
-			} catch (e) {
-				return when.reject(e);
-			}
-
-			return result.done ? result.value : when(result.value, next, fail);
-		}
+		return run(generator, this, args || []);
 	}
+
+	/**
+	 * Helper to initiate the provided generator as a coroutine
+	 * @returns {*}
+	 */
+	function run(generator, thisArg, args) {
+		var stepper = new Stepper(next, error, generator.apply(thisArg, args));
+
+		return stepper.step('next', void 0);
+
+		function next(x) { return stepper.step('next', x); }
+		function error(e) { return stepper.step('throw', e); }
+	}
+
+	/**
+	 * Manages the process of stepping the provided iterator
+	 * @constructor
+	 */
+	function Stepper(next, error, iterator) {
+		this.next = next;
+		this.error = error;
+		this.iterator = iterator;
+	}
+
+	Stepper.prototype.step = function(action, x) {
+		try {
+			return this._continue(action, x);
+		} catch (e) {
+			return when.reject(e);
+		}
+	};
+
+	Stepper.prototype._continue = function(action, x) {
+		var result = this.iterator[action](x);
+		return result.done ? result.value : when(result.value, this.next, this.error);
+	};
+
+	return {
+		lift: lift,
+		call: call,
+		apply: apply
+	};
+
 });
 }(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
